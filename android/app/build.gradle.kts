3f03a1c024421eb02536aab0792211e62f1dd06b
Wessley Turner <gredsavage@gmail.com>
2026-08-29 16:43:27 -0500

    Trigger Android APK build 0.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "com.ether404.allknowledge"; compileSdk = 35
-    defaultConfig { applicationId = "com.ether404.allknowledge"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "0.2.0" }
+    defaultConfig { applicationId = "com.ether404.allknowledge"; minSdk = 26; targetSdk = 35; versionCode = 3; versionName = "0.3.0" }
 }
 
 kotlin { jvmToolchain(17) }
