99fa5ab6f728ecb6fd93f7ca4b37cffc88190aae
Wessley Turner <gredsavage@gmail.com>
2026-08-30 10:51:00 -0500

    Use stable signing key and bump APK version for updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,26 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
-android { namespace = "com.ether404.allknowledge"; compileSdk = 35
-    defaultConfig { applicationId = "com.ether404.allknowledge"; minSdk = 26; targetSdk = 35; versionCode = 3; versionName = "0.3.0" }
+android {
+    namespace = "com.ether404.allknowledge"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.ether404.allknowledge"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 4
+        versionName = "0.4.0"
+    }
+    signingConfigs {
+        create("stableDebug") {
+            storeFile = rootProject.file("datapipeline-debug.jks")
+            storePassword = "datapipeline"
+            keyAlias = "datapipeline"
+            keyPassword = "datapipeline"
+        }
+    }
+    buildTypes {
+        getByName("debug") { signingConfig = signingConfigs.getByName("stableDebug") }
+    }
 }
 
 kotlin { jvmToolchain(17) }
