6fb6e6d0486fa78fff449436428c3ad516554968
Wessley Turner <gredsavage@gmail.com>
2026-08-31 02:38:07 -0500

    Bump Android version for UI/data navigation fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.ether404.allknowledge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.5.1"
     }
 }
 
