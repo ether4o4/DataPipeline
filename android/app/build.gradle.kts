7f05b191b23438492c4c9619081dae09d20d2998
Wessley Turner <gredsavage@gmail.com>
2026-08-31 03:53:22 -0500

    Lock APK signing for seamless updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,35 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "com.ether404.allknowledge"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.ether404.allknowledge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.1"
+        versionCode = 7
+        versionName = "0.5.2"
+    }
+
+    signingConfigs {
+        create("stable") {
+            val keystoreFile = rootProject.file("signing/datapipeline-release.keystore")
+            if (keystoreFile.exists()) {
+                storeFile = keystoreFile
+                storePassword = "datapipeline"
+                keyAlias = "datapipeline"
+                keyPassword = "datapipeline"
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("stable")
+        }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("stable")
+            isMinifyEnabled = false
+        }
     }
 }
 
