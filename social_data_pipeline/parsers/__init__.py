550aef903778796681a58fdb680eaf77983e04fc
Wessley Turner <gredsavage@gmail.com>
2026-09-01 19:20:35 -0500

    Register social platform parsers

---
+++ social_data_pipeline/parsers/__init__.py
@@ -0,0 +1,6 @@
+from .discord import DiscordParser
+from .facebook import FacebookParser
+from .instagram import InstagramParser
+from .snapchat import SnapchatParser
+
+__all__ = ["DiscordParser", "FacebookParser", "InstagramParser", "SnapchatParser"]
